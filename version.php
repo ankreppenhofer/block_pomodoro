d88fd7454101654eb3d1917ae009db639091a3fd
Anne <anne.kreppenhofer@tuwien.ac.at>
2025-09-02 12:11:37 +0200

    Forgot a line (my bad)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025090200;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025090200.01;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2025040800;        // Requires this Moodle version.
 $plugin->component = 'block_pomodoro';
-$plugin->component = 'block_rss_client'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_STABLE;
